c3c83e9227c6fa3c845eba7f810bd92df1168eb3
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2022-11-28 16:07:32 +0600

    delete unnecessary files

--- database/db.js
+++ database/db.js
@@ -4,17 +4,17 @@ const mysqlConfig = {
   // user: "tvcvlnrr_cit",
   // password: "m6w,b!jG2s=e",
   //------------
-  // host: "localhost",
-  // user: "root",
-  // database: "tvc",
-  // password: "password",
-  // port: 3306,
-  //-------------
   host: "localhost",
-  user: "exhortde_mh",
-  password: "em(.xwh@5^y.",
-  database: "exhortde_test",
-  port: "3306",
+  user: "root",
+  database: "tvc",
+  password: "password",
+  port: 3306,
+  //-------------
+  // host: "localhost",
+  // user: "exhortde_mh",
+  // password: "em(.xwh@5^y.",
+  // database: "exhortde_test",
+  // port: "3306",
 };
 
 export default mysqlConfig;
